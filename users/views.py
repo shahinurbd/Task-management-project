29d578879a2b7b625b070a4b0d38f6073dc42651
Muhammad Shahinur <shahinurislam728@gmail.com>
2025-03-15 18:51:14 +0600

    Practice-17.5

--- users/views.py
+++ users/views.py
@@ -24,22 +24,6 @@ def is_admin(user):
     return user.groups.filter(name='Admin').exists()
 
 
-# def sign_up(request):
-#     form = CustomRegistrationForm()
-#     if request.method == 'POST':
-#         form = CustomRegistrationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.set_password(form.cleaned_data.get('password1'))
-#             user.is_active = False
-#             user.save()
-#             messages.success(
-#                 request, 'A Confirmation mail sent. Please check your email')
-#             return redirect('sign-in')
-
-#         else:
-#             print("Form is not valid")
-#     return render(request, 'registration/register.html', {"form": form})
 
 class SignUp(View):
 
@@ -70,20 +54,6 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         next_url = self.request.GET.get('next')
         return next_url if next_url else super().get_success_url()
-
-    
-# def activate_user(request, user_id, token):
-#     try:
-
-#         user = User.objects.get(id=user_id)
-#         if default_token_generator.check_token(user, token):
-#             user.is_active =True
-#             user.save()
-#             return redirect('sign-in')
-#         else:
-#             return HttpResponse("Invalid id or token")
-#     except:
-#         return HttpResponse("User not found")
     
 
 class ActiveUser(View):
